5e8d1c499e50b6c1d610b6616bcfafb8bfcc7bff
devlucasalmd <lucas.amaral.almd@gmail.com>
2023-05-09 15:57:51 -0300

    Arrumando Arquivos

--- Level3/primeirapagina/src/App.js
+++ Level3/primeirapagina/src/App.js
@@ -1,28 +1,14 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import HelloWorld from "./components/HelloWorld" //importando o componente
 
 function App() {
-  const name = "Lucas" // Normalmente vem de algum lugar (Banco de Dados)
-  const newName = name.toUpperCase()
 
-
-  function sum(a, b){
-    var a,b;
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
-  return (
-    <div className="App">  
-        <p>Olá Mundo</p> // Precisa estar dentro de um Fragmento
-        <p>Olá {name}</p>
-        <p>Soma: {sum(2,2)}</p>
-        <img src={url} alt="IMagem"/> //url dinamica
-        <HelloWorld/>
-    </div>
-  );
+  return(
+  <>
+  
+  
+  </>
+  )
 }
 
 export default App;
