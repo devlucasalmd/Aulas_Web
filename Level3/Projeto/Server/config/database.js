393540b096a54a63cba2d27e65540beed15fd094
devlucasalmd <lucas.amaral.almd@gmail.com>
2023-05-31 10:21:28 -0300

    Arrumei o erro

--- Level3/Projeto/Server/config/database.js
+++ Level3/Projeto/Server/config/database.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
     {
         dialect:'sqlite',
-        storage: '../db/database.sqlite'
+        storage: './db/database.sqlite'
     }
 )
  
